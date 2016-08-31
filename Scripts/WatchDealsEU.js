68aa3c6d3820e53404baf5686bbe93dd9e5d2172
Lachezar Vasilev <arsenal4life95@gmail.com>
2016-09-01 00:18:29 +0300

    Finalle

--- Scripts/WatchDealsEU.js
+++ Scripts/WatchDealsEU.js
@@ -9,12 +9,42 @@ const kinveyAuthHeaders = {
 };
 const guestUserHeader = {
     'Authorization': 'Kinvey 42c87b2c-dcc6-4ec5-a43f-ecb464c25ea0.4D/KSMnjRMQRvENIEeSmGxKaxSY6+3MVyPygEMfu7T0='
-}
+};
 
 function showInfo(message) {
     $('#infoBox').text(message);
     $('#infoBox').show();
-    setTimeout(function() { $('#infoBox').fadeOut()},3000);
+
+}
+function showInfo1(message) {
+    $('#infoBox1').text(message);
+    $('#infoBox1').show();
+
+}
+function showInfo3(message) {
+    $('#infoBox3').text(message);
+    $('#infoBox3').show();
+
+}
+function showInfo4(message) {
+    $('#infoBox4').text(message);
+    $('#infoBox4').show();
+
+}
+function showInfo5(message) {
+    $('#infoBox5').text(message);
+    $('#infoBox5').show();
+
+}
+function showInfo6(message) {
+    $('#infoBox6').text(message);
+    $('#infoBox6').show();
+
+}
+function showInfo7(message) {
+    $('#infoBox7').text(message);
+    $('#infoBox7').show();
+
 }
 
 function showError(errorMsg) {
@@ -32,8 +62,6 @@ function handleAjaxError(response) {
         errorMsg = response.responseJSON.description;
 
     console.log("Fail", errorMsg);
-
-    // showError(errorMsg);
 }
 
 function login(e) {
@@ -59,6 +87,7 @@ function login(e) {
         let userAuth = response._kmd.authtoken;
         sessionStorage.setItem('authToken', userAuth);
         window.location.reload();
+        showInfo()
     }
 }
 function listOffers() {
@@ -84,14 +113,6 @@ function loadOffersSuccess(Offers) {
     if (Offers.length == 0) {
         $('#Offers').text('No Offers to Display.');
     } else {
-        // let OffersTable = $('<table>')
-        // .append($('<tr>').append(
-        //     '<th>Title</th>',
-        //     '<th>FullName</th>',
-        //     '<th>PhoneNumber</th>',
-        //     '<th>Price</th>',
-        //     '<th>Description</th>')
-        // );
         for (let offer of Offers) {
             $("#Offers").append([
                 '<div class="col-xs-4 offer">',
@@ -100,154 +121,143 @@ function loadOffersSuccess(Offers) {
                 '   <div class="text-info offer-description">' + offer.Description + '</div>',
                 '   <div class="row">',
                 '      <div class="col-xs-8 offer-phone">' + offer.PhoneNumber + '</div>',
+                '   </div>',
                 '      <div class="col-xs-4 offer-price">' + offer.Price + '</div>',
                 '   </div>',
                 '</div>'
             ].join(''));
-
-            // OffersTable.append($('<tr>').append(
-            //     $('<td>').text(offer.Title),
-            //     $('<td>').text(offer.FullName),
-            //     $('<td>').text(offer.PhoneNumber),
-            //     $('<td>').text(offer.Price),
-            //     $('<td>').text(offer.Description))
-            // );
         }
-        // $('#Offers').append(OffersTable);
     }
 }
-
-function logout() {
-    sessionStorage.clear();
-    window.location.reload()
-}
-
-function ShowHomePage() {
-    let _that = this;
-    let templateUrl;
-
-    $.get(templateUrl, function (template) {
-        let renderedWrapper = Mustache.render(template, null);
-        $(_that._wrapperSelector).html(renderedWrapper);
-
-        $.get('Home.html', function (template) {
-            let rendered = Mustache.render(template, null);
-            $(_that._mainContentSelector).html(rendered);
-        })
-    })
-
-}
-
-function register(e) {
-    e.preventDefault();
-
-    const kinveyRegisterUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/";
-
-    let user = {
-        name: $('#name').val(),
-        username: $('#username').val(),
-        password: $('#password').val(),
-        repassword: $('#confirm').val()
-    };
-
-    if (user.password !== user.repassword) {
-        //TODO: Display error
-        return;
+    function logout() {
+        sessionStorage.clear();
+        window.location.reload();
+        showInfo1();
     }
 
-    if (user.name === "" ||
-        user.password === "" ||
-        user.username === "") {
-        //TODO: Display error
-        return;
+
+    function register(e) {
+        e.preventDefault();
+
+        const kinveyRegisterUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/";
+
+        let user = {
+            name: $('#name').val(),
+            username: $('#username').val(),
+            password: $('#password').val(),
+            repassword: $('#confirm').val()
+        };
+
+        let requestData = {
+            username: user.username,
+            password: user.password,
+            name: user.name
+        };
+        if (requestData.username.length < 5) {
+            showInfo5()
+            return;
+        }
+        if (requestData.password !== user.repassword ) {
+            showInfo3()
+            return;
+        }
+        if (user.name === "" ||
+            user.password === "" ||
+            user.username === "") {
+        showInfo4()
+            return;
+        }
+
+        $.ajax({
+            method: "POST",
+            url: kinveyRegisterUrl,
+            headers: kinveyAuthHeaders,
+            data: requestData,
+            success: registerSuccess,
+            error: handleAjaxError
+        });
+
+        function registerSuccess(response) {
+            let userAuth = response._kmd.authtoken;
+            sessionStorage.setItem('authToken', userAuth);
+            console.log('User registration successful.');
+            showInfo6()
+        }
     }
 
-    let requestData = {
-        username: user.username,
-        password: user.password,
-        name: user.name,
-    };
+    function isUserLoggedIn() {
+        return sessionStorage.hasOwnProperty('authToken');
+    }
 
-    $.ajax({
-        method: "POST",
-        url: kinveyRegisterUrl,
-        headers: kinveyAuthHeaders,
-        data: requestData,
-        success: registerSuccess,
-        error: handleAjaxError
+    function createOffer(e) {
+        e.preventDefault();
+
+        const kinveyOffersUrl = kinveyBaseUrl + "appdata/" + kinveyAppKey + "/Offers";
+        const userAuthHeader = {
+            "Authorization": "Kinvey " + sessionStorage.authToken
+        };
+
+        if (!isUserLoggedIn()) {
+            //TODO: Show error
+            return;
+        }
+
+        let OfferData = {
+            Title: $('#OfferTitle').val(),
+            FullName: $('#OfferFullName').val(),
+            PhoneNumber: $('#OfferPhoneNumber').val(),
+            Price: $('#OfferPrice').val(),
+            Description: $('#OfferDescription').val()
+        };
+        if (
+            $('#OfferTitle').val() === "" ||
+        $('#OfferFullName').val() === "" ||
+            $('#OfferPhoneNumber').val() === "" ||
+            $('#OfferPrice').val() === "" ||
+            $('#OfferDescription').val() === "") {
+            showInfo4()
+            return;
+        }
+        $.ajax({
+            method: "POST",
+            url: kinveyOffersUrl,
+            headers: userAuthHeader,
+            data: OfferData,
+            success: createOfferSuccess,
+            error: handleAjaxError
+        });
+
+        function createOfferSuccess(response) {
+            console.log("Offer Created");
+            showInfo7()
+        }
+
+    }
+
+
+    $("#register-form").on('submit', register);
+    $("#login-nav").on('submit', login);
+    $("#createOffer-form").on('submit', createOffer);
+
+    var page = page || '';
+
+    if (page === 'viewoffers') {
+        listOffers();
+    }
+
+    $(function () {
+        $navbar = $('.navbar-right');
+
+        if (isUserLoggedIn()) {
+            var logoutElement = $('<li><a href="Home.html">Log Out</a></li>');
+            logoutElement.on('click', logout);
+            $navbar.html(logoutElement);
+        }
     });
 
-    function registerSuccess(response) {
-        let userAuth = response._kmd.authtoken;
-        sessionStorage.setItem('authToken', userAuth);
-        console.log('User registration successful.');
-    }
-}
+    $(function () {
+        if (isUserLoggedIn()) {
+            $navbar = $('#posttheoffer').show();
 
-function isUserLoggedIn() {
-    return sessionStorage.hasOwnProperty('authToken');
-}
-
-function createOffer(e) {
-    e.preventDefault();
-
-    const kinveyOffersUrl = kinveyBaseUrl + "appdata/" + kinveyAppKey + "/Offers";
-    const userAuthHeader = {
-        "Authorization": "Kinvey " + sessionStorage.authToken
-    };
-
-    if (!isUserLoggedIn()) {
-        //TODO: Show error
-        return;
-    }
-
-    let OfferData = {
-        Title: $('#OfferTitle').val(),
-        FullName: $('#OfferFullName').val(),
-        PhoneNumber: $('#OfferPhoneNumber').val(),
-        Price: $('#OfferPrice').val(),
-        Description: $('#OfferDescription').val()
-    };
-    $.ajax({
-        method: "POST",
-        url: kinveyOffersUrl,
-        headers: userAuthHeader,
-        data: OfferData,
-        success: createOfferSuccess,
-        error: handleAjaxError
+        }
     });
-
-    function createOfferSuccess(response) {
-        console.log("Offer Created");
-        showInfo('Offer created.');
-    }
-
-}
-
-
-
-$("#register-form").on('submit', register);
-$("#login-nav").on('submit', login);
-$("#createOffer-form").on('submit', createOffer);
-
-var page = page || '';
-
-if (page === 'viewoffers') {
-    listOffers();
-}
-
-$(function () {
-    $navbar = $('.navbar-right');
-
-    if (isUserLoggedIn()) {
-        var logoutElement = $('<li><a href="Home.html">Log Out</a></li>');
-        logoutElement.on('click', logout);
-        $navbar.html(logoutElement);
-    }
-});
-
-$(function () {
-    if (isUserLoggedIn()){
-        $navbar = $('#posttheoffer').show();
-    }
-});
